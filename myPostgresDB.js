fe4b7a82dab7432732a0d124ad63492477a1de21
Matt Mortensen <mortensenm1205@gmail.com>
2018-02-03 03:15:36 -0700

    was able to get my server to post and get data to and from my db, just a couple more tweaks

--- myPostgresDB.js
+++ myPostgresDB.js
@@ -5,23 +5,54 @@ const pool = new Pool({
   connectionString: connectionString
 });
 
-const values = ["Bobby Boi", "bob@bobby.com"];
+function postData(name, email) {
+  pool.query('INSERT INTO users (full_name, email) VALUES ($1, $2)', [name, email], (err, res) => {});
+}
 
-pool.query('INSERT INTO users (full_name, email) VALUES ($1, $2)', values, (err, res) => {});
+function getData(table) {
+  return new Promise((resolve, reject) => {
 
-pool.query('SELECT * FROM users', (err, res) => {
-  console.log(err, res.rows[1]);
-  pool.end();
-});
+    pool.query(`SELECT * FROM ${table}`, (err, res) => {
+
+      if (err) {
+        reject(new Error('whoops'));
+      }
+
+      resolve(res);
+    });
+  });
+}
+
+module.exports = {
+  postData: postData,
+  getData: getData
+}
 
 
-const client = new Client({
-  database: 'fullstack_form_db'
-});
 
-client.connect();
 
-client.query('SELECT * FROM users', (err, res) => {
-  console.log(err, res.rows[0]);
-  client.end();
-})
+
+
+
+
+
+
+
+// const values = ["Bobby Boi", "bob@bobby.com"];
+
+// pool.query('SELECT * FROM users', (err, res) => {
+//   console.log(err, res.rows[1]);
+//   pool.end();
+// });
+
+//
+// const client = new Client({
+//   database: 'fullstack_form_db'
+// });
+//
+// client.connect();
+//
+// client.query('SELECT * FROM users', (err, res) => {
+//   console.log(err, res.rows[0]);
+//   client.end();
+// })
--- route.js
+++ route.js
@@ -1,17 +1,22 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var pg = require('./myPostgresDB.js');
 
 app.use(bodyParser.urlencoded({ extended:true }));
 app.use('/', express.static('public'));
 
 app.post('/', (req, res) => {
-  res.send('text');
-  console.log(req.body);
+  pg.postData(req.body.full_name, req.body.email)
+  res.send('post recieved');
 });
 
 app.get('/passed', (req, res) => {
-  res.send('post recieved');
+  pg.getData('users').then((result) => {
+    res.send(`Hello ${result.rows[5].full_name}, your email is now under: ${result.rows[5].email}`);
+  }).catch((err) => {
+    console.log(err);
+  });
 })
 
 app.listen(9000, () => {
