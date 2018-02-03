97eaeeea3b64e26ffe3fdf9bcaa577fe07c6d917
Matt Mortensen <mortensenm1205@gmail.com>
2018-02-03 02:33:35 -0700

    now included pg to read from Client connection with one programmatic option

--- myPostgresDB.js
+++ myPostgresDB.js
@@ -1,4 +1,4 @@
-const { Pool } = require('pg');
+const { Pool, Client } = require('pg');
 const connectionString = 'postgresql://mattmortensen:blackshoes01@localhost/fullstack_form_db';
 
 const pool = new Pool({
@@ -10,6 +10,18 @@ const values = ["Bobby Boi", "bob@bobby.com"];
 pool.query('INSERT INTO users (full_name, email) VALUES ($1, $2)', values, (err, res) => {});
 
 pool.query('SELECT * FROM users', (err, res) => {
-  console.log(err, res.rows);
+  console.log(err, res.rows[1]);
   pool.end();
 });
+
+
+const client = new Client({
+  database: 'fullstack_form_db'
+});
+
+client.connect();
+
+client.query('SELECT * FROM users', (err, res) => {
+  console.log(err, res.rows[0]);
+  client.end();
+})
