ac19617eaf741842f11ef4ec834f859ad261e50b
Matt Mortensen <mortensenm1205@gmail.com>
2018-02-02 21:18:08 -0700

    installed body-parser, was able to maniuplate recieved data through req.body

--- route.js
+++ route.js
@@ -1,10 +1,13 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 
+app.use(bodyParser.urlencoded({ extended:true }));
 app.use('/', express.static('public'));
 
 app.post('/', (req, res) => {
   res.send('post recieved');
+  console.log(req.body);
 });
 
 app.listen(9000, () => {
