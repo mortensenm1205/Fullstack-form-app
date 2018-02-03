038891478369434c5138c733e7397a3b3773ed65
Matt Mortensen <mortensenm1205@gmail.com>
2018-02-02 21:53:08 -0700

    added logic to ajax call so that if res returns true to change windows location which express fires of a get request to that new location

--- route.js
+++ route.js
@@ -6,9 +6,13 @@ app.use(bodyParser.urlencoded({ extended:true }));
 app.use('/', express.static('public'));
 
 app.post('/', (req, res) => {
-  res.send('post recieved');
+  res.send('text');
   console.log(req.body);
 });
+
+app.get('/passed', (req, res) => {
+  res.send('post recieved');
+})
 
 app.listen(9000, () => {
   console.log('Running on PORT 9000');
