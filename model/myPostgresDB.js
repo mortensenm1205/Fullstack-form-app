4658302e7e7da3b59088767b718957791c281216
Matt Mortensen <mortensenm1205@gmail.com>
2018-02-04 08:54:00 -0700

    updated dir structure. included Procfile. updated pck

---
+++ model/myPostgresDB.js
@@ -0,0 +1,60 @@
+const { Pool, Client } = require('pg');
+const connectionString = 'postgresql://mattmortensen:blackshoes01@localhost/fullstack_form_db';
+
+const pool = new Pool({
+  connectionString: connectionString
+});
+
+function postData(name, email) {
+  pool.query('INSERT INTO users (full_name, email) VALUES ($1, $2)', [name, email], (err, result) => {
+    console.log(result);
+  });
+}
+
+function getData(table) {
+  return new Promise((resolve, reject) => {
+
+    pool.query(`SELECT * FROM ${table}`, (err, res) => {
+
+      if (err) {
+        reject(new Error('whoops'));
+      }
+
+      resolve(res);
+    });
+  });
+}
+
+module.exports = {
+  postData: postData,
+  getData: getData
+}
+
+
+
+
+
+
+
+
+
+
+
+// const values = ["Bobby Boi", "bob@bobby.com"];
+
+// pool.query('SELECT * FROM users', (err, res) => {
+//   console.log(err, res.rows[1]);
+//   pool.end();
+// });
+
+//
+// const client = new Client({
+//   database: 'fullstack_form_db'
+// });
+//
+// client.connect();
+//
+// client.query('SELECT * FROM users', (err, res) => {
+//   console.log(err, res.rows[0]);
+//   client.end();
+// })
---
+++ route.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var pg = require('./model/myPostgresDB.js');
+
+app.use(bodyParser.urlencoded({ extended:true }));
+app.use('/', express.static('public'));
+
+app.post('/', (req, res) => {
+  pg.postData(req.body.full_name, req.body.email);
+  res.send('post recieved');
+});
+
+app.get('/passed', (req, res) => {
+  pg.getData('users').then((result) => {
+    res.send(`Hello ${result.rows[result.rows.length - 1].full_name}, your email is now under: ${result.rows[result.rows.length - 1].email}`);
+  }).catch((err) => {
+    console.log(err);
+  });
+})
+
+app.listen(process.env.PORT, '0.0.0.0', () => {
+  console.log('Running on PORT ');
+})
