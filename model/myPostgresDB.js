9b6148c5a6560fe9f8149045d62e5fa458b9b170
Matt Mortensen <mortensenm1205@gmail.com>
2018-02-07 19:09:49 -0700

    refactored code to work on localhost

--- model/myPostgresDB.js
+++ model/myPostgresDB.js
@@ -5,10 +5,15 @@ const pool = new Pool ({
   connectionString: connectionString
 })
 
-
+function postData(value1, value2, cb) {
+  pool.connect(function (err, client, done) {
+    client.query('INSERT INTO users (full_name, email) VALUES ($1, $2)', [value1, value2], function(err, results) {
+      cb(err, results);
+    });
+  });
+};
 
 
 module.exports = {
-  postData: postData,
-  getData: getData
+  postData: postData
 }
--- route.js
+++ route.js
@@ -1,11 +1,13 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var pg = require('./model/myPostgresDB.js');
 
 app.use(bodyParser.urlencoded({ extended:true }));
 app.use('/', express.static('public'));
 
 app.post('/', (req, res) => {
+  pg.postData(req.body.full_name, req.body.email, function(err, results) {});
   res.send('post recieved');
 });
 
